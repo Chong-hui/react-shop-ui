3292612e3fdd01926712148c5001e85d0d6e66e9
Hui Lin <huilyn.thl@gmail.com>
2022-03-28 22:42:07 +0800

    revert

--- src/auth/AuthHistory.js
+++ src/auth/AuthHistory.js
@@ -12,6 +12,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const onRedirectCallback = (appState) => {
     history.push(appState?.returnTo || window.location.pathname);
   };
+  
 
   return (
     <Auth0Provider
